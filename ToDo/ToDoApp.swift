4a817a217da1b664b263c048ccaa80695b31c82e
Hiroki Kashihara <himawarisun1331@gmail.com>
2025-04-06 08:21:35 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Hiroki Kashihara on 2025/04/06.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
